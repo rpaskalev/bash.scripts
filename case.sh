e9f18a6186e422a364443201c737fcddb526c036
MahmudRahim <makhmud77@yahoo.com>
2021-10-09 14:42:36 -0400

    please check

--- case.sh
+++ case.sh
@@ -3,7 +3,7 @@
 
 #When called, this script has the purpose of translating state initials into full state name.
 
-read "State initials " 
+read -p "State initials " 
 
 case $REPLY in
 
@@ -40,7 +40,7 @@ echo "enetr valid state initials"
 esac
 
 if [[ $REPLY =~ ^OH|NY|VA|AZ|MD|FL|TX|CA|DC ]]
-then ;
+then 
 echo "YEY! you fixed it"
 elif [[ $REPLY !=  @(def|OH|NY|VA|AZ|MD|FL|TX|CA|DC) ]] 
 then ;
