fe8f571ec0afa1355dedd2289228061bf21b0a81
MahmudRahim <makhmud77@yahoo.com>
2021-10-09 14:48:22 -0400

    check this one now

--- curl.sh
+++ curl.sh
@@ -2,6 +2,6 @@
 
 url="http://mirror.filearena.net/pub/speed/SpeedTest_16MB.dat?_ga=2.103161856.1428494463.1587349458-1731585439.1587349458"
 
-curl ${url} -o TestFile
+curl ${url} -o TestFile 1
 
 #curl -I ${url}
